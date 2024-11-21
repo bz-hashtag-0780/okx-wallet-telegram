3a8721d301ba84ad25959648b0c585c4f1c3e7d9
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-11-21 14:18:29 +0100

    Update WalletConnectButton.tsx

--- components/WalletConnectButton.tsx
+++ components/WalletConnectButton.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from 'react';
 import { OKXUniversalConnectUI, THEME } from '@okxconnect/ui';
@@ -115,7 +116,7 @@ const WalletConnectButton = () => {
 
 	return (
 		<div>
-			{connected ? (
+			{address ? (
 				<div>
 					<p>Connected: {address}</p>
 					<button
